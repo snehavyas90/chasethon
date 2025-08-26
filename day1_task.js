ba0f1753088ab039a78fcf98f6d63172ecdd7781
Harshita-0928 <harshitapatidar7242@gmail.com>
2025-08-26 19:10:56 +0530

    Add files via upload

---
+++ day1_task.js
@@ -0,0 +1,11 @@
+const student = {
+  name: "Harshita",
+  age: 19,
+  Enrollment_no: "CSE2025-045",
+  branch: "Computer Science",
+ };
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log("Roll No:", student.Enrollment_no);
+console.log("Branch:", student.branch);
+
